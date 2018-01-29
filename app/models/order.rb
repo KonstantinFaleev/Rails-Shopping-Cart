3f2b8fbcf7b1d24242f3c66d64cdb8d1f3e0e179
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-01-29 23:27:09 +0200

    validates order, picture product

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  validates :name, :comment, :phone, :email, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ApplicationRecord
   validates :name, :description, :picture, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :name, uniqueness: true
+  validates :picture, allow_blank: true, format: {
+      with: %r{\.(gif|jpg|png)\Z}i,
+      message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
