a87a4330d6d68a720483f2dac15bc97e2275d81b
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-01-29 23:24:03 +0200

    validates product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   has_many :line_items
   has_many :orders, through: :line_items
+
+  validates :name, :description, :picture, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :name, uniqueness: true
 end
